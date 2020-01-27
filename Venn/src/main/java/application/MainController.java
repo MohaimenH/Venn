7023596e181dd9cecc8bde77aba3cafe5d9573d3
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2020-01-27 11:38:05 -0500

    testing things

--- Venn/src/main/java/application/MainController.java
+++ Venn/src/main/java/application/MainController.java
@@ -1,9 +1,40 @@
 package application;
 
+import java.awt.Button;
+import java.awt.TextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class MainController {
-	public void generateRandom(ActionEvent event) {
-		
+	private TextArea Right;
+	private TextArea Left;
+	private Button submit;
+	 @FXML
+	    private URL location;
+	     
+	    @FXML
+	    private ResourceBundle resources;
+	
+	public  MainController() {
 	}
+	 @FXML
+	    private void initialize() 
+	    {
+	    }
+	 @FXML
+
+	 public void setOther(ActionEvent e) {
+		 System.out.print("test");
+		 String thing= Left.getText();
+		 Right.setText(thing);
+	 }
+	 @FXML
+
+	 private void printOutput() 
+	    {
+	        Right.setText(Left.getText());
+	    }
 }
