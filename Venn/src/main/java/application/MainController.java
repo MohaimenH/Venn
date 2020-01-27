fcd957225ff13aa6bc14ee36f30727be7b1d8c03
MohaimenH <hassan08@my.yorku.ca>
2020-01-27 00:47:20 -0500

    Started Working on Sets.

--- Venn/src/main/java/application/MainController.java
+++ Venn/src/main/java/application/MainController.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.util.*;
+
 import javafx.event.ActionEvent;
 
 public class MainController {
 	public void generateRandom(ActionEvent event) {
+		Set<String> newSet = new HashSet<String>();
+	
 		
 	}
 }
