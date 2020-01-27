4fed36a1ee5092314505f92dea7d3f2b6f20c26c
MohaimenH <hassan08@my.yorku.ca>
2020-01-27 13:13:45 -0500

    Trying to Make Change.

--- Venn/src/main/java/application/Main.java
+++ Venn/src/main/java/application/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			// Making a change for lab.
+			//Changes
 	}
 	
 	public static void main(String[] args) {
