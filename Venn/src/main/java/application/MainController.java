228d6231554547ebe5b1c5b2e4e8c29402dd1b1c
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2020-01-29 12:13:24 -0500

    some early drag and drop functionality.

--- Venn/src/main/java/application/MainController.java
+++ Venn/src/main/java/application/MainController.java
@@ -3,19 +3,25 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class MainController {
+	Boolean selected=false;
+	String temp;
+	@FXML
+	private ListView<String> holder;
 	 @FXML
 	private TextField inputText;
 	 @FXML
 	private TextArea right;
 	 @FXML 
-	private TextArea left;
+	private ListView<String> left;
 	 @FXML
 	private Button submit;
 	 @FXML
@@ -36,6 +42,29 @@ public class MainController {
 
 	 private void printOutput() 
 	    {
-	        right.setText(inputText.getText());
+		 	String place=inputText.getText();
+
+		 holder.getItems().add(place);
 	    }
+	 @FXML
+	 private Boolean detectDrop() {
+		 selected=true;
+		 return selected;
+	 }
+	 @FXML
+	 private void detactDrag() {
+		 //what we can do is detect when an item is selected, then depending on weather its selected we 
+		 //detect where the mouse is and set that text to the appropriate text area
+		 selected=false;
+             temp=holder.getSelectionModel().getSelectedItem();
+             System.out.print(temp);
+
+	 }
+	 @FXML
+	 private void detectLeft() {
+		 Boolean detection=detectDrop();
+		 if(detection) {
+			 left.getItems().add(temp);
+		 }
+	 }
 }
