8d23fa2197697372310558b9dc16a6f4a4877285
Linigari-Luke-216307639 <46363148+Linigari-Luke-216307639@users.noreply.github.com>
2020-02-01 19:46:24 -0500

    input things

--- Venn/src/main/java/application/MainController.java
+++ Venn/src/main/java/application/MainController.java
@@ -7,6 +7,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -15,40 +16,57 @@ public class MainController {
 	Boolean selected=false;
 	String temp;
 	int index;
+	//////////////////
 	@FXML
-	private ListView<String> holder;
+	private Button left_label_button;
+	@FXML
+	private Button right_label_button;
+	@FXML
+	private Label leftLabel;
+	@FXML
+	private Label rightLabel;
+	@FXML
+	private TextField right_label_input;
+	@FXML
+	private TextField left_label_input;
+	//=============================================//
+
 	 @FXML
-	private TextField inputText;
+	 private ListView<String> holder;
 	 @FXML
-	private ListView<String> right;
+	 private TextField inputText;
+	 @FXML
+	 private TextArea inputArea;
+	 @FXML
+	 private ListView<String> right;
 	 @FXML 
-	private ListView<String> left;
+	 private ListView<String> left;
 	 @FXML
 	 private ListView<String> middle;
 	 @FXML
-	private Button submit;
-	 @FXML
+	 private Button submit;
+	 //=============================================//
+	    @FXML
 	    private URL location;
 	     
 	    @FXML
 	    private ResourceBundle resources;
-	
+	 //=============================================//
 	public  MainController() {}
-
-	
 	 @FXML
 	    private void initialize() 
 	    {
 	    }
-	
-	 @FXML
+	//=============================================//
 
-	 private void printOutput() 
+	 @FXML
+	 private void printOutput()//gets input text and adds it to master list
 	    {
 		 	String place=inputText.getText();
 		 holder.getItems().add(place);
 		 inputText.clear();
 	    }
+	//=============================================//drag and drop detection
 	 @FXML
 	 private void detectDrop() {
 		selected =true;
@@ -62,7 +80,6 @@ public class MainController {
 	 }
 	 @FXML
 	 private void detectLeft() {
-		 
 		 if(selected) {
 			 left.getItems().add(temp);
 			 holder.getItems().remove(index);
@@ -82,7 +99,6 @@ public class MainController {
 	 }
 	 @FXML
 	 private void detectMiddle() {
-		 
 		 if(selected) {
 			 middle.getItems().add(temp);
 			 holder.getItems().remove(index);
@@ -90,4 +106,19 @@ public class MainController {
 			 
 		 }
 	 }
+	//=============================================//label customization
+	 @FXML
+	 private void setLeftLabel() {
+		 String left=left_label_input.getText();
+		 leftLabel.setText(left);
+		 inputText.clear();
+	 }
+	 @FXML
+	 private void setRightLabel() {
+		 String right=right_label_input.getText();
+		 leftLabel.setText(right);
+		 inputText.clear();
+	 }
+	 //=============================================//
+
 }
